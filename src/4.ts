2846486df058dd52bf1d897cbbd1ccddb3625c80
Olena <33802160+bilcha@users.noreply.github.com>
2023-12-05 21:20:26 -0800

    homework update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+  private signature = Math.random();
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {};
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  public tenants: Person[] = [];
+  comeIn(person: Person): void {
+    if (this.door) { 
+      this.tenants.push(person)
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key): void {
+    this.door = this.key.getSignature() === key.getSignature();
+  }
+
+}
 const key = new Key();
 
 const house = new MyHouse(key);
